d478de24af1bb5ad8b2a0b9bb6a72adacabc0c0f
AlanThinks <a@alanthinks.com>
2018-07-26 15:01:53 -0400

    Fixing Contact Form Bug

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,55 +1,63 @@
 $(function() {
-    $("input,select,textarea").not("[type=submit]").jqBootstrapValidation({
-        preventSubmit: true,
+  $("input,select,textarea")
+    .not("[type=submit]")
+    .jqBootstrapValidation({
+      preventSubmit: true,
 
-        submitError: function($form, event, errors) {
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            let thisForm = event.target.getAttribute('id');
-            console.log(thisForm)
-            let name = $('#' + thisForm).find("input.name-input").val();
-            let company = $('#' + thisForm).find("input.company-input").val();
-            let email = $('#' + thisForm).find("input.email-input").val();
-            let phone = $('#' + thisForm).find("input.phone-input").val();
-            let message = $('#' + thisForm).find("textarea.textarea").val();
+      submitError: function($form, event, errors) {},
+      submitSuccess: function($form, event) {
+        event.preventDefault(); // prevent default submit behaviour
+        // get values from FORM
+        let thisForm = event.target.getAttribute("id");
+        let name = $("#" + thisForm)
+          .find("input.name-input")
+          .val();
+        let company = $("#" + thisForm)
+          .find("input.company-input")
+          .val();
+        let email = $("#" + thisForm)
+          .find("input.email-input")
+          .val();
+        let phone = $("#" + thisForm)
+          .find("input.phone-input")
+          .val();
+        let message = $("#" + thisForm)
+          .find("textarea.textarea")
+          .val();
 
-            $.ajax({
-                url: "https://formspree.io/code@alanthinks.com",
-                type: "POST",
-                dataType: 'json',
-                data: {
-                    name: name,
-                    company: company,
-                    phone, phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function(data) {
-                    if(data.error){
-                    }
-                    else if(data.success){
-	                    // Success message
-                        $('#send-message-modal').modal();
-	                    //clear all fields
-                        $('#' + thisForm).trigger("reset");
-                        //close contact modal
-                        $('#contact-me-modal').modal("hide");
-					}
-                }
-            });
-
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        }
+        $.ajax({
+          url: "https://formspree.io/code@alanthinks.com",
+          type: "POST",
+          dataType: "json",
+          data: {
+            name: name,
+            company: company,
+            phone,
+            phone,
+            email: email,
+            message: message
+          },
+          cache: false,
+          success: function(data) {
+            if (data.error) {
+            } else if (data.success) {
+              // Success message
+              $("#send-message-modal").modal();
+              //clear all fields
+              $("#" + thisForm).trigger("reset");
+              //close contact modal
+              $("#contact-me-modal").modal("hide");
+            }
+          }
+        });
+      },
+      filter: function() {
+        return $(this).is(":visible");
+      }
     });
 });
 
-
 /*When clicking on Full hide fail/success boxes */
-$('#form-first-name').focus(function() {
-    $('#success').html('');
+$("#form-first-name").focus(function() {
+  $("#success").html("");
 });
