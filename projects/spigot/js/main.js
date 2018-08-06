ccead49f16245ec870abb07fedd30471e09998b9
AlanThinks <a@alanthinks.com>
2018-08-05 23:46:09 -0400

    Landing Page Project Responsive Update

--- projects/spigot/js/main.js
+++ projects/spigot/js/main.js
@@ -1,4 +1,24 @@
-if ($(window).width() < 350) {
+if ($(window).width() < 400 && $(window).height() > 800) {
+  //   $(function() {
+  //     var button = $(".barcelona-cta");
+  //     $(window).scroll(function() {
+  //       var scroll = $(window).scrollTop();
+  //       // Was going to use all jQuery, but ended up using a mix of vanilla JS because I was experimenting with a possible animation;
+  //       //   if (scroll >= 100) {
+  //       //     button.addClass("barcelona-cta-fix");
+  //       //   } else {
+  //       //     button.removeClass("barcelona-cta-fix");
+  //       //   }
+  //       if (scroll >= 95) {
+  //         document.getElementsByClassName("barcelona-cta")[0].style.position =
+  //           "static";
+  //       } else {
+  //         document.getElementsByClassName("barcelona-cta")[0].style.position =
+  //           "fixed";
+  //       }
+  //     });
+  //   });
+} else if ($(window).width() < 350) {
   $(function() {
     var button = $(".barcelona-cta");
     $(window).scroll(function() {
@@ -18,9 +38,7 @@ if ($(window).width() < 350) {
       }
     });
   });
-}
-
-if ($(window).width() < 451 && $(window).width() > 350) {
+} else if ($(window).width() < 451 && $(window).width() > 350) {
   $(function() {
     var button = $(".barcelona-cta");
     $(window).scroll(function() {
@@ -31,7 +49,7 @@ if ($(window).width() < 451 && $(window).width() > 350) {
       //   } else {
       //     button.removeClass("barcelona-cta-fix");
       //   }
-      if (scroll >= 125) {
+      if (scroll >= 60) {
         document.getElementsByClassName("barcelona-cta")[0].style.position =
           "static";
       } else {
