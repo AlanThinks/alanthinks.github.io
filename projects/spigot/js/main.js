428a69f3eca2c6db4b5e2d12265b12c6e8b590e8
AlanThinks <a@alanthinks.com>
2018-08-05 22:36:13 -0400

    Landing Page Project Responsive Update

---
+++ projects/spigot/js/main.js
@@ -0,0 +1,43 @@
+if ($(window).width() < 350) {
+  $(function() {
+    var button = $(".barcelona-cta");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      // Was going to use all jQuery, but ended up using a mix of vanilla JS because I was experimenting with a possible animation;
+      //   if (scroll >= 100) {
+      //     button.addClass("barcelona-cta-fix");
+      //   } else {
+      //     button.removeClass("barcelona-cta-fix");
+      //   }
+      if (scroll >= 105) {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "static";
+      } else {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "fixed";
+      }
+    });
+  });
+}
+
+if ($(window).width() < 451 && $(window).width() > 350) {
+  $(function() {
+    var button = $(".barcelona-cta");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      // Was going to use all jQuery, but ended up using a mix of vanilla JS because I was experimenting with a possible animation;
+      //   if (scroll >= 100) {
+      //     button.addClass("barcelona-cta-fix");
+      //   } else {
+      //     button.removeClass("barcelona-cta-fix");
+      //   }
+      if (scroll >= 155) {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "static";
+      } else {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "fixed";
+      }
+    });
+  });
+}
