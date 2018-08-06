1ac1843b90f0cfdc97b82e8cb8b5cd9e13f4fa47
AlanThinks <a@alanthinks.com>
2018-08-05 23:49:48 -0400

    Landing Page Project Responsive Update

--- projects/spigot/js/main.js
+++ projects/spigot/js/main.js
@@ -1,4 +1,24 @@
-if ($(window).width() < 400 && $(window).height() > 800) {
+if ($(window).width() < 450 && $(window).height() > 799) {
+  $(function() {
+    var button = $(".barcelona-cta");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      // Was going to use all jQuery, but ended up using a mix of vanilla JS because I was experimenting with a possible animation;
+      //   if (scroll >= 100) {
+      //     button.addClass("barcelona-cta-fix");
+      //   } else {
+      //     button.removeClass("barcelona-cta-fix");
+      //   }
+      if (scroll >= 50) {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "static";
+      } else {
+        document.getElementsByClassName("barcelona-cta")[0].style.position =
+          "fixed";
+      }
+    });
+  });
+} else if ($(window).width() < 400 && $(window).height() > 800) {
   //   $(function() {
   //     var button = $(".barcelona-cta");
   //     $(window).scroll(function() {
