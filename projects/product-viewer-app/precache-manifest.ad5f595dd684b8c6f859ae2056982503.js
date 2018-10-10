9a0b408edd64b95db1e484d1df3475cbbdb85dd1
AlanThinks <a@alanthinks.com>
2018-10-09 21:52:10 -0400

    Adding Product Viewer Mini-App (In Progress)

---
+++ projects/product-viewer-app/precache-manifest.ad5f595dd684b8c6f859ae2056982503.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62b8a1f59ac85f63e95b",
+    "url": "/projects/product-viewer-app/static/js/runtime~main.62b8a1f5.js"
+  },
+  {
+    "revision": "5050dfc70aab3fa4f861",
+    "url": "/projects/product-viewer-app/static/js/main.5050dfc7.chunk.js"
+  },
+  {
+    "revision": "e1c69298af96ac6074bc",
+    "url": "/projects/product-viewer-app/static/js/1.e1c69298.chunk.js"
+  },
+  {
+    "revision": "5050dfc70aab3fa4f861",
+    "url": "/projects/product-viewer-app/static/css/main.bf71a707.chunk.css"
+  },
+  {
+    "revision": "b2a8b1d732b4f88a8e61626c28a80c24",
+    "url": "/projects/product-viewer-app/index.html"
+  }
+];
