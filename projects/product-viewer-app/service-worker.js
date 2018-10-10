21a2ac8c1f9d21a1f74f00d91a6944bb754c3bd8
AlanThinks <a@alanthinks.com>
2018-10-09 21:36:35 -0400

    Product Detail View

--- projects/product-viewer-app/service-worker.js
+++ projects/product-viewer-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a6a536427ff3844e5c1a4e48b31490d.js"
+  "/projects/product-viewer-app/precache-manifest.603f70b0455e45b5411b7269f5e0f779.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/projects/product-viewer-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
