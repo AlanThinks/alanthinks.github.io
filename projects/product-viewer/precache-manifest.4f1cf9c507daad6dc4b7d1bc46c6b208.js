6595f15e0d78265134b686d9ae736050493875a9
AlanThinks <a@alanthinks.com>
2018-10-09 22:27:48 -0400

    Product Viewer Mini-App (in progres)

---
+++ projects/product-viewer/precache-manifest.4f1cf9c507daad6dc4b7d1bc46c6b208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c79558f16d5d3e056a7f",
+    "url": "/projects/product-viewer/static/js/runtime~main.c79558f1.js"
+  },
+  {
+    "revision": "26f9c60e90850ee37927",
+    "url": "/projects/product-viewer/static/js/main.26f9c60e.chunk.js"
+  },
+  {
+    "revision": "8e7405902b81f4cd7ed9",
+    "url": "/projects/product-viewer/static/js/1.8e740590.chunk.js"
+  },
+  {
+    "revision": "26f9c60e90850ee37927",
+    "url": "/projects/product-viewer/static/css/main.2bc53363.chunk.css"
+  },
+  {
+    "revision": "581eb1047eb2985e9a4423887a6b85c4",
+    "url": "/projects/product-viewer/index.html"
+  }
+];
