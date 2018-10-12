976c540f83405a2155001668b6de4b0411d5c7cb
AlanThinks <a@alanthinks.com>
2018-10-12 10:33:04 -0400

    Added logo to loading spinner

--- js/main.js
+++ js/main.js
@@ -1,124 +1,122 @@
 /* main js */
 $(document).ready(function() {
-  "use strict";
+  "use strict"
 
   function preload() {
     var $preloader = $("#page-preloader"),
-      $spinner = $preloader.find(".spinner-loader");
+      $spinner = $preloader.find(".spinner-loader")
     $(window).on("load", function() {
-      $spinner.fadeOut();
-      $preloader.delay(500).fadeOut("slow");
-    });
+      $spinner.fadeOut("fast")
+      $preloader.fadeOut("slow")
+      // $preloader.delay(500).fadeOut("slow")
+    })
   }
-  preload();
+
+  preload()
 
   //fixed header
   function fixedHeader(wSc) {
     if (wSc > 10) {
-      $(".header").addClass("active");
+      $(".header").addClass("active")
     } else {
-      $(".header").removeClass("active");
+      $(".header").removeClass("active")
     }
   }
 
   //fixed sidebar
-  var sidebarCheck = false;
+  var sidebarCheck = false
   if ($("div").is(".sidebar-form")) {
-    sidebarCheck = true;
-    var sidebar = $(".sidebar-form");
-    var sidebarPos,
-      sidebarHeight,
-      singleProjectSection,
-      singleSectionBottom,
-      ww;
-    var posChecked = true;
+    sidebarCheck = true
+    var sidebar = $(".sidebar-form")
+    var sidebarPos, sidebarHeight, singleProjectSection, singleSectionBottom, ww
+    var posChecked = true
     $(window).load(function() {
-      ww = $(window).width();
-      sidebarHeight = sidebar.outerHeight();
-      singleProjectSection = $(".single-project-section");
+      ww = $(window).width()
+      sidebarHeight = sidebar.outerHeight()
+      singleProjectSection = $(".single-project-section")
       singleSectionBottom =
         singleProjectSection.offset().top +
         singleProjectSection.outerHeight() -
-        60;
+        60
 
       $(window).on("resize", function() {
-        ww = $(window).width();
+        ww = $(window).width()
         if (ww > 991 && posChecked) {
-          sidebarPos = sidebar.offset().top;
-          posChecked = false;
+          sidebarPos = sidebar.offset().top
+          posChecked = false
         }
-        sidebarHeight = sidebar.outerHeight();
+        sidebarHeight = sidebar.outerHeight()
         singleSectionBottom =
           singleProjectSection.offset().top +
           singleProjectSection.outerHeight() -
-          60;
-      });
+          60
+      })
 
-      $(window).trigger("resize");
-    });
+      $(window).trigger("resize")
+    })
   }
   function fixedSidebar(wSc) {
     if (
       wSc > sidebarPos - 85 &&
       wSc + 85 + sidebarHeight < singleSectionBottom
     ) {
-      sidebar.addClass("fixed-sidebar");
-      sidebar.removeClass("absolute-sidebar");
+      sidebar.addClass("fixed-sidebar")
+      sidebar.removeClass("absolute-sidebar")
     } else if (wSc + 85 + sidebarHeight > singleSectionBottom) {
-      sidebar.removeClass("fixed-sidebar");
-      sidebar.addClass("absolute-sidebar");
+      sidebar.removeClass("fixed-sidebar")
+      sidebar.addClass("absolute-sidebar")
     } else {
-      sidebar.removeClass("fixed-sidebar");
-      sidebar.removeClass("absolute-sidebar");
+      sidebar.removeClass("fixed-sidebar")
+      sidebar.removeClass("absolute-sidebar")
     }
   }
 
   //use fixedSidebar and fixedHeader
   $(window).on("scroll", function() {
-    var wSc = $(window).scrollTop();
-    fixedHeader(wSc);
+    var wSc = $(window).scrollTop()
+    fixedHeader(wSc)
 
     if (sidebarCheck) {
-      fixedSidebar(wSc);
+      fixedSidebar(wSc)
     }
-  });
+  })
   $(window).load(function() {
-    $(window).trigger("scroll");
-  });
+    $(window).trigger("scroll")
+  })
 
   //change lang cookie
   function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
+    var nameEQ = name + "="
+    var ca = document.cookie.split(";")
     for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      var c = ca[i]
+      while (c.charAt(0) == " ") c = c.substring(1, c.length)
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
     }
-    return null;
+    return null
   }
   function setLang() {
-    var cookieLang = readCookie("lang");
+    var cookieLang = readCookie("lang")
     if (cookieLang) {
       $(".change-lng ul li a").each(function() {
         if ($(this).text() == cookieLang) {
           $(this)
             .parent("li")
-            .addClass("active");
+            .addClass("active")
         }
-      });
+      })
     } else {
-      document.cookie = "lang=" + $(".change-lng ul li:first a").text();
-      $(".header .change-lng ul li:first").addClass("active");
-      $(".mob-menu-wrapper .change-lng ul li:first").addClass("active");
+      document.cookie = "lang=" + $(".change-lng ul li:first a").text()
+      $(".header .change-lng ul li:first").addClass("active")
+      $(".mob-menu-wrapper .change-lng ul li:first").addClass("active")
     }
   }
-  setLang();
+  setLang()
   $(".change-lng ul li a").on("click", function(event) {
-    event.preventDefault();
-    document.cookie = "lang=" + $(this).text();
-    window.location = "index.html";
-  });
+    event.preventDefault()
+    document.cookie = "lang=" + $(this).text()
+    window.location = "index.html"
+  })
 
   //init isotope
   var $grid = $(".projects-wrapper").isotope({
@@ -133,39 +131,39 @@ $(document).ready(function() {
     masonry: {
       columnWidth: ".project-item"
     }
-  });
+  })
   $grid.imagesLoaded().progress(function() {
-    $grid.isotope("layout");
-  });
+    $grid.isotope("layout")
+  })
   //isotope filter
   $(".projects-filter ul li").on("click", function() {
-    $(".projects-filter ul li").removeClass("active");
-    $(this).addClass("active");
-    var filterValue = $(this).attr("data-filter");
-    $(".projects-wrapper").isotope({ filter: filterValue });
-  });
+    $(".projects-filter ul li").removeClass("active")
+    $(this).addClass("active")
+    var filterValue = $(this).attr("data-filter")
+    $(".projects-wrapper").isotope({ filter: filterValue })
+  })
 
   //init gradient
   if ($("div").is(".first-screen") || $("div").is(".mob-menu-wrapper")) {
-    drawGradient();
+    drawGradient()
   }
 
   //open google map in new window
   $(".map-link").on("click", function(event) {
-    event.preventDefault();
+    event.preventDefault()
     window.open(
       "https://www.google.com.ua/maps/place/" + $(this).text(),
       "_blank"
-    );
-  });
+    )
+  })
 
   //scroll to anchor
   if ($("header").is(".header-home")) {
     $('.main-menu ul li a[href*="#"], .mobile-menu ul li a[href*="#"]').on(
       "click",
       function(event) {
-        event.preventDefault();
-        var anchor = $(this).attr("href");
+        event.preventDefault()
+        var anchor = $(this).attr("href")
         if ($(anchor).position()) {
           $("html, body")
             .stop()
@@ -174,10 +172,10 @@ $(document).ready(function() {
                 scrollTop: $(anchor).offset().top
               },
               800
-            );
+            )
         }
       }
-    );
+    )
   }
 
   // if($('section').is('.top-main-section')){
@@ -194,98 +192,98 @@ $(document).ready(function() {
 
   // Animate Smooth Scroll
   $("#projects-big-btn").on("click", function() {
-    const projects = $("#projects").position().top;
+    const projects = $("#projects").position().top
 
     $("html, body").animate(
       {
         scrollTop: projects
       },
       900
-    );
-  });
+    )
+  })
   $("#contact-big-btn").on("click", function() {
-    const contactSection = $("#contact-section").position().top;
+    const contactSection = $("#contact-section").position().top
 
     $("html, body").animate(
       {
         scrollTop: contactSection
       },
       900
-    );
-  });
+    )
+  })
 
   $("#top-left-logo").on("click", function() {
-    const topMainSection = $("#top-main-section").position().top;
+    const topMainSection = $("#top-main-section").position().top
 
     $("html, body").animate(
       {
         scrollTop: topMainSection
       },
       900
-    );
-  });
+    )
+  })
 
   $("#alanthinks-portfolio-project").on("click", function() {
-    const topMainSection = $("#top-main-section").position().top;
+    const topMainSection = $("#top-main-section").position().top
 
     $("html, body").animate(
       {
         scrollTop: topMainSection
       },
       900
-    );
-  });
+    )
+  })
 
   $("#see-live-project-btn").on("click", function() {
-    const liveProject = $("#live-project").position().top;
+    const liveProject = $("#live-project").position().top
 
     $("html, body").animate(
       {
         scrollTop: liveProject
       },
       900
-    );
-  });
+    )
+  })
 
   //open bootstrap modal from modal
   $(document).on("hidden.bs.modal", ".modal", function() {
     if ($(".modal:visible").length) {
-      $(document.body).addClass("modal-open");
-      $(document.body).css({ paddingRight: scrollWidth() });
-      $("header").css("padding-right", scrollWidth());
+      $(document.body).addClass("modal-open")
+      $(document.body).css({ paddingRight: scrollWidth() })
+      $("header").css("padding-right", scrollWidth())
     } else {
-      $(document.body).css({ paddingRight: 0 });
-      $("header").css({ paddingRight: 0 });
+      $(document.body).css({ paddingRight: 0 })
+      $("header").css({ paddingRight: 0 })
     }
-  });
+  })
 
   //bootstrap modal fix for fixed header
   function scrollWidth() {
-    var div = document.createElement("div");
-    div.style.overflowY = "scroll";
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.visibility = "hidden";
-    document.body.appendChild(div);
-    var paddingRight = div.offsetWidth - div.clientWidth;
-    document.body.removeChild(div);
+    var div = document.createElement("div")
+    div.style.overflowY = "scroll"
+    div.style.width = "50px"
+    div.style.height = "50px"
+    div.style.visibility = "hidden"
+    document.body.appendChild(div)
+    var paddingRight = div.offsetWidth - div.clientWidth
+    document.body.removeChild(div)
 
-    return paddingRight;
+    return paddingRight
   }
   $(".modal").on("show.bs.modal", function() {
-    $("header").css("padding-right", scrollWidth());
-  });
+    $("header").css("padding-right", scrollWidth())
+  })
   $(".modal").on("hidden.bs.modal", function() {
-    $(".header").css("padding-right", "0");
-  });
+    $(".header").css("padding-right", "0")
+  })
 
   //mobile-menu
   $(".mobile-btn, .close-mob-menu").on("click", function() {
-    $(".mob-menu-wrapper").toggleClass("active");
-  });
+    $(".mob-menu-wrapper").toggleClass("active")
+  })
   $(".mobile-menu ul li a").on("click", function() {
-    $(".mob-menu-wrapper").removeClass("active");
-  });
+    $(".mob-menu-wrapper").removeClass("active")
+  })
 
   //init highcharts
   if ($("div").is("#highchart")) {
@@ -316,10 +314,10 @@ $(document).ready(function() {
             " - " +
             this.point.name.toLowerCase() +
             "</span>"
-          );
+          )
         }
       }
-    });
+    })
   }
 
   //init custom scroll
@@ -334,6 +332,6 @@ $(document).ready(function() {
 
   //init share buttons
   if ($("span").is("#sharing-links")) {
-    $("#sharing-links").socialLikes();
+    $("#sharing-links").socialLikes()
   }
-});
+})
