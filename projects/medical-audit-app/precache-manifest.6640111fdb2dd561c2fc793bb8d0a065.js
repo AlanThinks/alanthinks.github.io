538743b94ecc897525cc916294a2d6f31d8323a5
AlanThinks <a@alanthinks.com>
2018-10-26 14:10:51 -0400

    Adding 'Medical Audit App'

---
+++ projects/medical-audit-app/precache-manifest.6640111fdb2dd561c2fc793bb8d0a065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e4ac6c07667cb2649ae",
+    "url": "/projects/medical-audit-app/static/js/runtime~main.0e4ac6c0.js"
+  },
+  {
+    "revision": "aea60055b313384a8545",
+    "url": "/projects/medical-audit-app/static/js/main.aea60055.chunk.js"
+  },
+  {
+    "revision": "7a5ca52efdf6fcb008e7",
+    "url": "/projects/medical-audit-app/static/js/1.7a5ca52e.chunk.js"
+  },
+  {
+    "revision": "aea60055b313384a8545",
+    "url": "/projects/medical-audit-app/static/css/main.70cdb6ee.chunk.css"
+  },
+  {
+    "revision": "c330127c9427405890d5787acc5d3729",
+    "url": "/projects/medical-audit-app/index.html"
+  }
+];
