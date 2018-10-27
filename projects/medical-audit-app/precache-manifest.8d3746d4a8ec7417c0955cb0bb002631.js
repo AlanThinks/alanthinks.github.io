76b791739d2ee8fe7eeac14c9bc6ea22cca9e11e
AlanThinks <a@alanthinks.com>
2018-10-26 20:47:26 -0400

    Updating Responsiveness on Medical Dashboard App

--- projects/medical-audit-app/precache-manifest.8d3746d4a8ec7417c0955cb0bb002631.js
+++ projects/medical-audit-app/precache-manifest.8d3746d4a8ec7417c0955cb0bb002631.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/projects/medical-audit-app/static/js/runtime~main.0e4ac6c0.js"
   },
   {
-    "revision": "58c87fabe8885dcd6a80",
-    "url": "/projects/medical-audit-app/static/js/main.58c87fab.chunk.js"
+    "revision": "f3416f1aadf079e701cf",
+    "url": "/projects/medical-audit-app/static/js/main.f3416f1a.chunk.js"
   },
   {
     "revision": "054136513faf33c2c911",
     "url": "/projects/medical-audit-app/static/js/1.05413651.chunk.js"
   },
   {
-    "revision": "58c87fabe8885dcd6a80",
-    "url": "/projects/medical-audit-app/static/css/main.0f130a03.chunk.css"
+    "revision": "f3416f1aadf079e701cf",
+    "url": "/projects/medical-audit-app/static/css/main.59cd61e9.chunk.css"
   },
   {
-    "revision": "15a922403f3e2648c6b03a824a97dc47",
+    "revision": "29441d53c9a583b673dd95fa56bdfdb5",
     "url": "/projects/medical-audit-app/index.html"
   }
 ];
--- projects/medical-audit-app/service-worker.js
+++ projects/medical-audit-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/medical-audit-app/precache-manifest.8f5180d35dbac498bd1ec2d8f327e45e.js"
+  "/projects/medical-audit-app/precache-manifest.8d3746d4a8ec7417c0955cb0bb002631.js"
 );
 
 workbox.clientsClaim();
