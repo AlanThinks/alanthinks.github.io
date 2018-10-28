a17f05b0bc00d85d9bf78319cae9dfbcdb846bbe
AlanThinks <a@alanthinks.com>
2018-10-28 09:34:54 -0400

    Making TaskItem subcomponent & improving responsiveness in Medical Audit App

---
+++ projects/medical-audit-app/precache-manifest.906386a1f2b235e262160e600738c9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e4ac6c07667cb2649ae",
+    "url": "/projects/medical-audit-app/static/js/runtime~main.0e4ac6c0.js"
+  },
+  {
+    "revision": "3a5a23716a6af12ddc7f",
+    "url": "/projects/medical-audit-app/static/js/main.3a5a2371.chunk.js"
+  },
+  {
+    "revision": "21da4745199c0aa1c652",
+    "url": "/projects/medical-audit-app/static/js/1.21da4745.chunk.js"
+  },
+  {
+    "revision": "3a5a23716a6af12ddc7f",
+    "url": "/projects/medical-audit-app/static/css/main.a0c6883a.chunk.css"
+  },
+  {
+    "revision": "98915b143c39c8e7f149bb66286b59ca",
+    "url": "/projects/medical-audit-app/index.html"
+  }
+];
--- projects/medical-audit-app/service-worker.js
+++ projects/medical-audit-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/medical-audit-app/precache-manifest.7cba2e87876b5ca8681c1effebd14529.js"
+  "/projects/medical-audit-app/precache-manifest.906386a1f2b235e262160e600738c9e0.js"
 );
 
 workbox.clientsClaim();
