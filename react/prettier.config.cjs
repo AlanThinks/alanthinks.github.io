a072e38f4a0cf66324772b1d181aef727321b2f7
AlanThinks <a@alanthinks.com>
2025-10-18 11:45:50 -0400

    /react folder set-up

---
+++ react/prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  proseWrap: "preserve"
+};
